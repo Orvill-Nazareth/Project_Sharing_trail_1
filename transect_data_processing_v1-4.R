52b8fa34519c58a5d996773853e0dd6c137a2e72
Orvill-Nazareth <nazareth.orvill@gmail.com>
2020-05-17 22:25:58 +0530

    corrected the distance sampling part so the entire code works as intended.

--- transect_data_processing_v1-4.R
+++ transect_data_processing_v1-4.R
@@ -10,7 +10,7 @@ library(magrittr)
 #load the files as variables and load the boundary shapefile----
 datasheet <- "C:/mydatasheet.xlsx"
 excel_sheets(datasheet)
-sheet_DS <- "sheet containaing direct sighting data"
+sheet_DS <- "sheet containing direct sighting data"
 sheet_pellet <- "sheet containing pellet plots data"
 sheet_h.dist_veg <- "sheet containing 400m interval data (human dist. and vegetation)"
 shapefile_dir <- "path/of/shapefile/folder"
@@ -152,17 +152,26 @@ leaflet() %>%
 
 #sort data for distance----
 #convert bearings to angle----
-DS_raw_bearing <- within (DS_raw, {sighting_angle <- ifelse( ifelse(abs(`Compass Bearing Animal`-`Compass Bearing Walk`)>90,
-                                                                    360-abs(`Compass Bearing Animal`-`Compass Bearing Walk`),
-                                                                    abs(`Compass Bearing Animal`-`Compass Bearing Walk`))>90,
-                                                             90,
-                                                             ifelse(abs(`Compass Bearing Animal`-`Compass Bearing Walk`)>90,
-                                                                    360-abs(`Compass Bearing Animal`-`Compass Bearing Walk`),
-                                                                    abs(`Compass Bearing Animal`-`Compass Bearing Walk`)))})
-
-#format rows----
-blank_transects <- transect_raw[!transect_raw$`Transect ID` %in% sighting_raw$`Transect ID`,]
-distance <- rbind(sighting_raw, blank_transects)
-
+#check the absolute difference between animal bearing and walk bearing call it 'x'.
+#if x is greater than 90, as this may happen when working with bearings greater than 270 and lesser than 90, then subract x from 360 and call this value 'y'.
+#if y is strill greater than 90, then use then use the back bearing of the walk and calculate the absolute difference between then animal bearing and the back bearing of the walk call it 'z'.
+Dist_S <- read_excel(datasheet, sheet = sheet_DS) %>% 
+  .[!is.na(.$`Sl. No.`),]
+Distance <-  read_excel(datasheet, sheet = sheet_DS) %>% 
+  .[!duplicated(.$`Transect ID`),] %>% 
+  .[!.$`Transect ID` %in% Dist_S$`Transect ID`,] %>% 
+  rbind(Dist_S,.) %>% 
+  .[with(.,order(`Transect ID`,`Transect Replicate/Day No.`)),]
+  within (., {sighting_angle <- ifelse(ifelse(ifelse(abs(`Compass Bearing Animal`-`Compass Bearing Walk`)>90,
+                                                     360-abs(`Compass Bearing Animal`-`Compass Bearing Walk`),
+                                                     abs(`Compass Bearing Animal`-`Compass Bearing Walk`))>90,
+                                              abs(`Compass Bearing Animal`-ifelse(`Compass Bearing Walk`-180<0,
+                                                          `Compass Bearing Walk`+180,
+                                                          `Compass Bearing Walk`-180)),
+                                              abs(`Compass Bearing Animal`-`Compass Bearing Walk`))>90,
+                                       360-(abs(`Compass Bearing Animal`-ifelse(`Compass Bearing Walk`-180<0,
+                                                               `Compass Bearing Walk`+180,
+                                                               `Compass Bearing Walk`-180))),
+                                       abs(`Compass Bearing Animal`-`Compass Bearing Walk`))})
 
 # points to lines code borrowed from https://stackoverflow.com/questions/42487700/how-to-create-a-polyline-for-each-row
